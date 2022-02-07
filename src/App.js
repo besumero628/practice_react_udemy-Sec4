b960b8efbbde9b5b4a9ef90290ea0de4789aeb0c
besumero628 <70096064+besumero628@users.noreply.github.com>
2022-02-07 18:48:17 +0900

    4-2 Css Modules

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { CssModules } from "./component/CssModules";
 import { InlineStyle } from "./component/inlineStyle";
 import "./styles.css";
 
@@ -5,6 +6,7 @@ export default function App() {
   return (
     <div className="App">
       <InlineStyle />
+      <CssModules />
     </div>
   );
 }
